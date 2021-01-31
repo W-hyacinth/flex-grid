116a382d294c77168490d31f540bd4533392a28b
MostOneBrush <w_hyacinth.choi@kakao.com>
2021-01-31 17:48:06 +0900

    1. width 값 재조정, 2. flex-basis 값 auto로 변경

--- src/components/FlexLayoutCardList1.vue
+++ src/components/FlexLayoutCardList1.vue
@@ -26,18 +26,17 @@ export default {
 .flex-list {
   display: flex;
   flex-wrap: wrap;
-  margin: -0.5% auto;
+  margin: 0 auto;
   /deep/ .card-wrap {
-    width: 49%;
+    width: calc((100% - (0.5% * 2) * 2) / 2); /* (100% - (여백값 * 2) * 1열에 들어갈 수량) / 1열에 들어갈 수량 */
     margin: 0.5%;
-    flex: 0 0 49%;
+    flex: 0 0 auto; /* flex-basis가 auto, width 값이 존재하는 경우 width 값을 가져옴 */
   }
 }
 @media screen and (min-width: 568px) and (max-width: 768px) {
   .flex-list {
     /deep/ .card-wrap {
-      width: 32%;
-      flex: 0 0 32%;
+      width: calc((100% - (0.5% * 2) * 3) / 3);
     }
   }
 }
@@ -45,8 +44,7 @@ export default {
   .flex-list {
     max-width: 960px;
     /deep/ .card-wrap {
-      width: 24%;
-      flex: 0 0 24%;
+      width: calc((100% - (0.5% * 2) * 4) / 4);
     }
   }
 }
