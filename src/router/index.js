cafe790b5ebd824586af0df212e35e92d6ba2d55
MostOneBrush <w_hyacinth.choi@kakao.com>
2021-01-31 19:14:18 +0900

    ellipsis가 포함된 다른 타입의 flex 카드 레이아웃 IE10+, chrome, safari, firefox 대응 후 업로드

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import FlexLayoutBasic from '@/components/FlexLayoutBasic'
 import FlexLayoutCardList1 from '@/components/FlexLayoutCardList1'
+import FlexLayoutCardList2 from '@/components/FlexLayoutCardList2'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/flex/list/1',
       name: 'FlexLayoutCardList1',
       component: FlexLayoutCardList1
+    },
+    {
+      path: '/flex/list/2',
+      name: 'FlexLayoutCardList2',
+      component: FlexLayoutCardList2
     }
   ]
 })
